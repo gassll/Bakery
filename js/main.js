252ece87532e5a5f71af198575dba1fa1aa8ab66
gassll <lizakonopatskaya@gmail.com>
2025-04-16 23:00:33 +0300

    видео

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+    const burger = document.getElementById('burger');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const closeBtn = document.getElementById('closeBtn');
+
+  burger.addEventListener('click', () => {
+        mobileMenu.classList.add('active');
+  });
+
+  closeBtn.addEventListener('click', () => {
+        mobileMenu.classList.remove('active');
+  });
+
+  document.querySelectorAll('.mobile-menu-links a').forEach(link => {
+        link.addEventListener('click', () => {
+            mobileMenu.classList.remove('active');
+        });
+  });
+
