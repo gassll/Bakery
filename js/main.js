4dfa14e2dfb05bb883c9e478e372dfff90e2d2a8
Your Name <you@example.com>
2025-04-21 09:44:21 +0300

    форма отправки

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobileMenu");
+const closeBtn = document.getElementById("closeBtn");
 
-const burger = document.getElementById('burger');
-const mobileMenu = document.getElementById('mobileMenu');
-const closeBtn = document.getElementById('closeBtn');
-
-burger.addEventListener('click', () => {
-      mobileMenu.classList.add('active');
+burger.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
 });
 
-closeBtn.addEventListener('click', () => {
-      mobileMenu.classList.remove('active');
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
 });
 
-document.querySelectorAll('.mobile-menu-links a').forEach(link => {
-      link.addEventListener('click', () => {
-            mobileMenu.classList.remove('active');
-      });
+document.querySelectorAll(".mobile-menu-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+  });
 });
+
+document
+  .getElementById("feedbackForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    // Здесь будет код для отправки данных на сервер
+
+    alert("Сообщение отправлено!");
+    this.reset();
+  });
