205e237cf9c231182c83ce912f5fb749834bb742
gassll <lizakonopatskaya@gmail.com>
2025-04-27 21:41:26 +0300

    немного лишнего кода убрала

--- js/main.js
+++ js/main.js
@@ -1,33 +1,37 @@
-const burger = document.getElementById("burger");
-const mobileMenu = document.getElementById("mobileMenu");
-const closeBtn = document.getElementById("closeBtn");
+document.addEventListener("DOMContentLoaded", function() {
+  const burger = document.getElementById("burger");
+  const mobileMenu = document.getElementById("mobileMenu");
+  const closeBtn = document.getElementById("closeBtn");
 
-burger.addEventListener("click", () => {
-  mobileMenu.classList.add("active");
-});
+  burger.addEventListener("click", () => {
+    mobileMenu.classList.add("active");
+  }, { passive: true });
 
-closeBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("active");
-});
-
-document.querySelectorAll(".mobile-menu-links a").forEach((link) => {
-  link.addEventListener("click", () => {
+  closeBtn.addEventListener("click", () => {
     mobileMenu.classList.remove("active");
+  }, { passive: true });
+
+  document.querySelectorAll(".mobile-menu-links a").forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.remove("active");
+    }, { passive: true });
   });
+
+  //отзывы обратная связь
+
+  const feedbackForm = document.getElementById("feedbackForm");
+  if (feedbackForm) {
+    feedbackForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      var message = document.getElementById("message").value;
+
+      // отправка данных на сервер
+
+      alert("Сообщение отправлено!");
+      this.reset();
+    });
+  }
 });
-
-//отзывы
-document
-  .getElementById("feedbackForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-
-    // Здесь будет код для отправки данных на сервер
-
-    alert("Сообщение отправлено!");
-    this.reset();
-  });
